1466fe18690fd832417e1624b56d16f50a11e3cd
Jason Bub <jason.bub@sbcglobal.net>
2017-11-19 12:25:31 -0600

    Completed the rejection sampling function. The pdf now ouptuts specified number of points as a matrix.

--- R/samp2D.R
+++ R/samp2D.R
@@ -6,5 +6,17 @@ samp2D <- function(fun,a,b,c,d,N) {
     ifelse(a <= x & x <= b & c <= y & y <= d, eval(g[[1]]), 0)
   }
   maxf <- optim(c(b/2,d/2),f,control = list(fnscale = -1))
-  maxf$value + .1
+  samples <- matrix(rep(0,2*N), nrow = N, ncol = 2)
+  i <- 0
+  while( i < N) {
+    psx <- runif(1,a,b)
+    psy <- runif(1,c,d)
+    testsamp <- runif(1,0, maxf$value + .1)
+    if (testsamp < f(c(psx,psy))) {
+      samples[i+1,1] <- psx
+      samples[i+1,2] <- psy
+      i <- i +1
+    }
+  }
+  samples
 }

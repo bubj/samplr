f66560dc46f553372fd18cb10ea91de73bded927
Jason Bub <jason.bub@sbcglobal.net>
2017-11-19 13:50:57 -0600

    Added comments to the 2D rejection sampler .R file.

--- R/samp2D.R
+++ R/samp2D.R
@@ -28,24 +28,24 @@
 
 
 samp2D <- function(fun,a,b,c,d,N) {
-  g <- parse(text = fun)
+  g <- parse(text = fun) # convert the input fun string into an expression
   f <- function(z) {
     x <- z[1]
     y <- z[2]
-    ifelse(a <= x & x <= b & c <= y & y <= d, eval(g[[1]]), 0)
+    ifelse(a <= x & x <= b & c <= y & y <= d, eval(g[[1]]), 0) # defining the pdf as a function where the input is a vector. The expression g is evaluated to get the equation.
   }
-  maxf <- optim(c(b/2,d/2),f,control = list(fnscale = -1))
-  samples <- matrix(rep(0,2*N), nrow = N, ncol = 2)
+  maxf <- optim(c(b/2,d/2),f,control = list(fnscale = -1)) # finds the maximum of the pdf
+  samples <- matrix(rep(0,2*N), nrow = N, ncol = 2) # creating a matrix to store the samples in
   i <- 0
-  while( i < N) {
-    psx <- runif(1,a,b)
-    psy <- runif(1,c,d)
-    testsamp <- runif(1,0, maxf$value + .1)
-    if (testsamp < f(c(psx,psy))) {
+  while( i < N) { # while loop that runs unil N accepted samples are found.
+    psx <- runif(1,a,b) # creating a potential x value to be tested
+    psy <- runif(1,c,d) # creating a potential y value to be tested
+    testsamp <- runif(1,0, maxf$value + .1) # creating a test sample using maxf$value to find the max of the pdf and adding .1 to account for errors in tolerance.
+    if (testsamp < f(c(psx,psy))) { # tests if the potential x and y values should be rejected. If kept, the x value is stored in the first column and the y value in the second of the samples matrix.
       samples[i+1,1] <- psx
       samples[i+1,2] <- psy
       i <- i +1
     }
   }
-  samples
+  samples # outputs the samples matrix.
 }

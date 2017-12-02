7a303641705e6d3dab46619be54a1019b01a7243
Jason Bub <jason.bub@sbcglobal.net>
2017-12-02 11:50:28 -0600

    Fixed a typo where the wrong function was called.

--- R/expsamp.R
+++ R/expsamp.R
@@ -4,7 +4,7 @@ expsamp <- function(f, g = NULL) {
     mean(samples)
   }
   else {
-    samples <- samp1D(f,10000)
+    samples <- samp2D(f,10000)
     values <- rep(0,10000)
     for (i in 1:10000) {
       values[i] <- g(c(samples[i,1],samples[i,2]))
